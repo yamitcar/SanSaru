3cfe67e1270664236ab657bb6b57cf36bbd6e5a4
yamit <yamit.cardenas@gmail.com>
2018-02-11 21:13:36 -0600

    generate model and scaffold for profile

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
+  resources :profiles
+  resources :agile_relations
+  resources :agiles
+  resources :genders
+  resources :sizes
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :movies do
-    collection do
-        get 'search'
-    end
-  end
   root 'application#hello'
 end
