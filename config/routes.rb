360dc9bbfa2be3ec642f867563adf4895e21fc72
yamit <yamit.cardenas@gmail.com>
2018-02-19 16:04:46 -0500

    change theme and submit profile pages

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   get 'static_pages/home'
-  get 'static_pages/help'
   resources :profiles
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#home'
