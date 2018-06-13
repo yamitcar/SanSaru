f5018c140921a908480db4d2d658a8ef13fb7104
yamit <yamit.cardenas@gmail.com>
2018-06-13 10:55:56 -0500

    adjust query

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -5,7 +5,7 @@ class ProfilesController < ApplicationController
 
   def index
     @postulados = User.where.not(profile_id: [nil]).where.not(id: current_user.id).
-        order("organizer DESC").paginate(:page => params[:page], :per_page => 10)
+        order("organizer DESC, created_at ASC").paginate(:page => params[:page], :per_page => 10)
   end
 
   # DELETE /profiles/1
