a834c0d847e97742500d9b0f2c6fe171f999b16c
Martin Salias <martin@salias.com.ar>
2019-01-08 01:39:07 -0300

    Fix Heroku app uri for mailer links

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,8 +89,8 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: 'san-saru.herokuapp.com' }
-  Rails.application.routes.default_url_options[:host] = 'san-saru.herokuapp.com'
+  config.action_mailer.default_url_options = { host: 'sansaru2019brc.herokuapp.com' }
+  Rails.application.routes.default_url_options[:host] = 'sansaru2019brc.herokuapp.com'
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
