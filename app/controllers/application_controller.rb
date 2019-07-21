12aa173e4432ebeedd23291b15869cf4fa2eef1e
yamit cardenas <yamit.cardenas@gmail.com>
2019-07-20 21:17:49 -0400

    MULTITENANT: ajustes en formularios de registro y traducciones

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,11 @@ class ApplicationController < ActionController::Base
   # protect_from_forgery with: :exception
   protect_from_forgery prepend: true
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_locale
+
+  def set_locale
+    I18n.locale = params[:locale] || I18n.default_locale
+  end
 
   protected
 
