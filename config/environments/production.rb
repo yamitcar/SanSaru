4a382b41b207e5c1096889704611e3ac74cc8ed1
yamit cardenas <yamit.cardenas@gmail.com>
2019-08-01 21:34:27 -0400

    MULTITENANT: issue with monos-sabios on heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -98,6 +98,7 @@ Rails.application.configure do
   config.action_mailer.default charset: 'utf-8'
 
   config.serve_static_assets = true
+  config.assets.compile = true
 
   config.action_mailer.smtp_settings = {
     address: ENV['SMTP_ADDR'],
